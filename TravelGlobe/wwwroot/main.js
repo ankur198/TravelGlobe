f1c2f10393e3b6e4397d42ed9ca84abb196e9d75
Ankur Nigam <ankur.nigam198@gmail.com>
2019-06-11 20:19:44 +0530

    edit

--- TravelGlobe/wwwroot/main.js
+++ TravelGlobe/wwwroot/main.js
@@ -18,6 +18,42 @@ const getPosition = () => {
     });
 };
 
+const addActivity = (id, position, description) => {
+    return new Promise((resolve, reject) => {
+        if (id && position && description) {
+            fetch(`/api/positions/${id}/${position}`, {
+                method: "post",
+                body: JSON.stringify({ description }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(data => {
+                if (data.status == 201) {
+                    resolve();
+                } else reject();
+            });
+        }
+    });
+};
+
+const updateActivity = (id,position,activity)=>{
+    return new Promise((resolve, reject) => {
+        if (id && position && activity) {
+            fetch(`/api/positions/${id}/${position}/${activity.id}`, {
+                method: "put",
+                body: JSON.stringify( activity ),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(data => {
+                if (data.status == 204) {
+                    resolve();
+                } else reject();
+            });
+        }
+    });
+}
+
 new Vue({
     el: "#app",
     data: {
@@ -27,7 +63,10 @@ new Vue({
         title: "",
         positions: [],
         editable: false,
-        showMajor: true
+        showMajor: true,
+
+        currentAct: null,
+        currentPos:null
     },
     mounted() {
         this.initData();
@@ -40,6 +79,10 @@ new Vue({
             this.title = Cookies.get("Title");
             this.editable = this.name === this.author;
 
+            await this.refreshPositions();
+        },
+
+        refreshPositions: async function() {
             this.positions = await getPosition();
         },
 
@@ -48,11 +91,26 @@ new Vue({
             let val = false;
             major.forEach(element => {
                 if (element == position) {
-                    console.log(element,element == position);
-                    val= true;
+                    val = true;
                 }
             });
             return val;
+        },
+
+        addActivity: async function(positionName) {
+            const element = document.querySelector(`#add${positionName}Text`);
+            //const btn = document.querySelector(`#add${positionName}Btn`)
+            //console.log(element.value);
+            await addActivity(this.id, positionName, element.value);
+            await this.refreshPositions();
+            element.value = "";
+        },
+
+        updateActivity: async function(e) {
+            await updateActivity(this.id,this.currentPos,this.currentAct)
+            await this.refreshPositions()
+            this.currentAct=null
+            this.currentPos=null
         }
     }
 });
